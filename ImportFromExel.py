2433cd73001deb9ede89b532a96f82b722485cd2
Mad Vitaliy <omelchenko.vitaliy1@gmail.com>
2022-11-08 10:59:56 +0200

    Draft Excel import was added

--- ImportFromExel.py
+++ ImportFromExel.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-
+from openpyxl.utils import *
 
 def FirstNotEmptyRow(sheet):
   for i, row in enumerate(sheet):
@@ -20,25 +20,39 @@ def NumberOfNotEmptyRow(sheet):
     if not all(cell.value is None for cell in row):
       counter += 1
   return counter
+
+def ReadColumns(sheet, colum_inds):
+  exel_column_inds = [get_column_letter(ind) for ind in colum_inds]
   
-
-wb = load_workbook('./test_data/words.xlsx')
-ws = wb.active
+  print(exel_column_inds)
 
 
-first_row_ind = FirstNotEmptyRow(ws)
-last_row_ind = LastNotEmptyRow(ws)
-number_of_not_empt = NumberOfNotEmptyRow(ws)
-print(first_row_ind)
-print(last_row_ind)
-print(number_of_not_empt)
+def ReadExcel(path):
+  wb = load_workbook(path)
+  ws = wb.active
+  
+  first_row_ind = FirstNotEmptyRow(ws)
+  last_row_ind = LastNotEmptyRow(ws)
+  number_of_not_empt = NumberOfNotEmptyRow(ws)
 
-#reading table header
-for i, cell in enumerate(ws[first_row_ind]): #indices starting from 1 suck
-  print(f'{i}:{cell.value}')
+  #reading table header
+  header_row_ind = first_row_ind
+  titles_inds = [i+1 for i, cell in enumerate(ws[header_row_ind]) if cell.value is not None]
+  titles = {}
+  for i in titles_inds:
+    cell = ws.cell(row=header_row_ind, column=i)
+    value = cell.value.strip().lower()
+    titles[i] = value 
 
-titles_inds = [i for i, cell in enumerate(ws[first_row_ind]) if cell.value is not None]
-print(titles_inds)
-
-for cell in ws[]
-
+  #reading all content
+  data =[]
+  for row_ind in range(header_row_ind+1, last_row_ind+1):
+    row_data = {}
+    for column_ind in titles_inds:
+      cell = ws.cell(row=row_ind, column=column_ind)
+      #print(cell)
+      value = cell.value
+      row_data[titles[column_ind]] = value
+    data.append(row_data)
+  
+  return [val for key,val in titles.items()], data
